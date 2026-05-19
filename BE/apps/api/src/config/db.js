de1d737f951ee07b8d231af23fb87a9aa1441ce1
mpman123 <mhp230306@gmail.com>
2026-05-19 13:47:24 +0700

    create seed in ipanmovie db

--- BE/apps/api/src/config/db.js
+++ BE/apps/api/src/config/db.js
@@ -1,7 +1,28 @@
 import mongoose from "mongoose";
 import { env } from "./env.js";
 
-export async function connectDatabase() {
-const connection =  await mongoose.connect(env.mongoUri);
-console.log(connection);
+const getConnectionString = () => {
+  const { mongoUser, mongoPassword, mongoCluster, mongoAppName, mongoDatabase } = env;
+  const encodedPassword = encodeURIComponent(mongoPassword);
+  return `mongodb+srv://${mongoUser}:${encodedPassword}@${mongoCluster}/${mongoDatabase}?appName=${mongoAppName}`;
 }
+
+export async function connectDatabase() {
+  try {
+    const connectionString = getConnectionString();
+    const connection = await mongoose.connect(connectionString);
+    if (connection.connections[0].readyState === 1) {
+      console.log('✅✅✅ MongoDB connected successfully');
+    }
+    return connection;
+  } catch (error) {
+    console.error("❌❌❌ error to connect mongodb");
+    return null;
+  }
+}
+
+export async function disconnectDatabase() {
+  await mongoose.disconnect();
+  console.log('✅✅✅ MongoDB disconnected successfully');
+}
+
